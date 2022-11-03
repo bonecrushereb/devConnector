d172e1c7abe528828b0afb6680aa7f0de08c1594
bonecrushereb <bonecrushereb@gmail.com>
2022-11-03 11:49:09 -0400

    auth route not working

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,15 +5,17 @@ module.exports = function(req, res, next) {
   const token = req.header('x-auth-token');
 
   if(!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied'});'
+    return res.status(401).json({ msg: 'No token, authorization denied'});
 
     try {
       const decoded = jwt.verify(token, config.get('jwtSecret'));
 
       req.user = decoded.user;
       next();
+
     }catch(err) {
-      res.status(401).json({ msg: 'Token is not valid'});
+      console.error('something wrong with auth middleware')
+      res.status(500).json({ msg: 'Server error'});
     }
   }
 }
