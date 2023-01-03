9d2c11567862bf1a6e9da65e2808a0730710cb17
bonecrushereb <bonecrushereb@gmail.com>
2023-01-03 13:32:58 -0500

    load user not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert'
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 import './App.css';
 
-const App = () => (
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
   <Provider store={store}>
     <Router>
       <Navbar />
@@ -23,6 +35,6 @@ const App = () => (
       </section>
     </Router>
   </Provider>
-);
+)};
 
 export default App;
