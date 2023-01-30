26d33efab30e1d0cd4400f30af21bc85a812b7e7
bonecrushereb <bonecrushereb@gmail.com>
2023-01-30 14:18:53 -0500

    updated profileabout

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: {profile, loading}, auth}) => {
@@ -23,6 +24,7 @@ const Profile = ({ getProfileById, profile: {profile, loading}, auth}) => {
         && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
         <div className="profile-grid my-1">
           <ProfileTop profile={profile} />
+          <ProfileAbout profile={profile} />
         </div>
         </section>
         }
