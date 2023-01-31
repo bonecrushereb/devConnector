ae0d800f45bfffe7808f8c55cbccc95f9d4e200e
bonecrushereb <bonecrushereb@gmail.com>
2023-01-31 12:05:24 -0500

    view education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: {profile, loading}, auth}) => {
@@ -25,6 +27,21 @@ const Profile = ({ getProfileById, profile: {profile, loading}, auth}) => {
         <div className="profile-grid my-1">
           <ProfileTop profile={profile} />
           <ProfileAbout profile={profile} />
+          <div className='profile-exp bg-white pp-2'>
+            <h2 className='text-primary'>Experience</h2>
+            {profile.experience.length > 0 ? (<section>
+              {profile.experience.map(experience => (
+                <ProfileExperience key={experience._id} experience={experience}/>
+              ))}
+            </section>) : (<h4>No experience credentials</h4>)}
+          </div>
+
+          <div className='profile-edu bg-white p-2'>
+            <h2 className='text-primary'>Education</h2>
+            {profile.education.map((education) => (
+              <ProfileEducation key={education._id} education={education} />
+            ))}
+          </div>
         </div>
         </section>
         }
