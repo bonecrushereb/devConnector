7e8444ceeac898c21608446d24b896bb5027a0d1
bonecrushereb <bonecrushereb@gmail.com>
2023-02-02 13:50:39 -0500

    create post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from './types';
 
 //Get posts
@@ -68,6 +69,24 @@ export const deletePost = id => async (dispatch) => {
       payload: id
     });
     dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+  });
+  }
+}
+
+//Add Post
+export const addPost = formData => async (dispatch) => {
+  try {
+    const res = await api.post('/api/posts', formData);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+    dispatch(setAlert('Post Added', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
